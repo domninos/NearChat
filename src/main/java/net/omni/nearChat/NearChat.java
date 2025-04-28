bae0ee160dfc6330929c9fbf51fa6c5427b552db
domninos <68453370+domninos@users.noreply.github.com>
2025-04-28 18:55:45 +0800

    Initial Commit
    Reviewed-by: domninos

---
+++ src/main/java/net/omni/nearChat/NearChat.java
@@ -0,0 +1,17 @@
+package net.omni.nearChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NearChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
